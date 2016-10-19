02c5cfbcb4e14f638ad0e07c6715ba7f703be73d
eliilek@gmail.com <eliilek@gmail.com>
2016-10-19 11:27:34 -0500

    heroku debugging

--- don_project/settings.py
+++ don_project/settings.py
@@ -121,4 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'))
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
