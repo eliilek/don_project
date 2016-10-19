70c4dfd99a8a9237cb24e66f13704e1ae13b0324
eliilek@gmail.com <eliilek@gmail.com>
2016-10-19 11:58:02 -0500

    Trying to find static admin files

--- don_project/settings.py
+++ don_project/settings.py
@@ -122,7 +122,7 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'),)
